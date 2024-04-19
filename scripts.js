d97b09f7c5f96b4c8378a1542c75936517c618e9
kiajohns <114971305+Kiara-Johnson@users.noreply.github.com>
2024-04-19 05:20:08 +0000

    base template

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// initialize initial variables with let
+
+
+// weapons and staff arrays of objects with names and power
+
+
+// array of location objects
+
+
+// functions
+// Town
+
+// Store
+
+// Library
+
+// Hunting Grounds
+
+// Occupation - mage, warrior, historian, scholar, rogue, etc
+
+// Enemies
